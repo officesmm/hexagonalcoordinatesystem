2c669215a7143b1c17198900f4de2e3a8c9abb1d
Soe Myat Min DESKTOP-5B01SMR <min.s@hundsun.co.jp>
2022-10-19 17:07:22 +0900

    some

--- src/main/java/org/vict/hexagonal/_core/InputController.java
+++ src/main/java/org/vict/hexagonal/_core/InputController.java
@@ -1,10 +1,12 @@
 package org.vict.hexagonal._core;
 
 import org.vict.hexagonal.model.coordinate.Vector2;
+import org.vict.hexagonal.model.playerinfo.Placement;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class InputController {
     BufferedReader br;
@@ -34,6 +36,15 @@ public class InputController {
                 System.out.println("Invalid Direction");
                 return RequestDirection();
         }
-
     }
+
+    public int GetPlacement(List<Placement> placementList) {
+        if (placementList.size() == 0) {
+            return 1;
+        } else {
+            return GetPlacement(placementList);
+        }
+    }
+
+
 }
